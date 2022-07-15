559246d2b934e94eab5249d51fb6b6d17404ed38
Sandwich7u7 <chompyxz27@gmail.com>
2022-07-15 19:35:42 -0300

    funciones ocultas B) ahre

--- src/components/IndProduct.js
+++ src/components/IndProduct.js
@@ -7,7 +7,7 @@ function Individual(props) {
             <div className="user">
                 <div className="user-info">
                     <h1>Producto {props.id}</h1>
-                    <span><img src={`http://localhost:3500/img/usersImages/${props.image}`}></img></span><br></br>
+                    <span><img src={`http://localhost:3500/img/usersImages/${props.image}`}alt={`no existe imagen`}></img></span><br></br>
                     
                     <span>Nombre: {props.name}</span><br/>
                     <span>Detalles: {props.details}</span><br/>
